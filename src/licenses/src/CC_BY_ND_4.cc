1969db44f067ede672d10597e70a93710225ea43
coder3101 <ashar786khan@gmail.com>
2019-10-22 00:55:39 +0530

    Applu licenser --update to this project

--- src/licenses/src/CC_BY_ND_4.cc
+++ src/licenses/src/CC_BY_ND_4.cc
@@ -1,21 +1,22 @@
-/**
- * Copyright (C) 2019 Ashar ashar786khan@gmail.com
- * 
- * This file is part of licenser.
- * 
- * licenser is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * licenser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with licenser.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+* Copyright (C) 2019- Mohammad Ashar Khan ashar786khan@gmail.com
+*  
+* This file is part of licenser.
+*  
+* licenser is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*  
+* licenser is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*  
+* You should have received a copy of the GNU General Public License
+* along with licenser.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "CC_BY_ND_4.hpp"
 
