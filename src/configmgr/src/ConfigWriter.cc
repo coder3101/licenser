f1946a4bc7cc136632f679c3e116fe968c928fd0
coder3101 <ashar786khan@gmail.com>
2019-10-22 01:00:29 +0530

    Create alpha release v0.1
    patch msvc compilation error
    
    Update .licenserrc and project name

--- src/configmgr/src/ConfigWriter.cc
+++ src/configmgr/src/ConfigWriter.cc
@@ -1,26 +1,30 @@
 /*
 * Copyright (C) 2019- Mohammad Ashar Khan ashar786khan@gmail.com
 *  
-* This file is part of licenser.
+* This file is part of Licenser.
 *  
-* licenser is free software: you can redistribute it and/or modify
+* Licenser is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *  
-* licenser is distributed in the hope that it will be useful,
+* Licenser is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *  
 * You should have received a copy of the GNU General Public License
-* along with licenser.  If not, see <http://www.gnu.org/licenses/>.
+* along with Licenser.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
 #include "ConfigWriter.hpp"
+
 #include <filesystem>
 #include <fstream>
+
+#define LICENSER_RC_DECORATOR \
+  "# ========================================================\n"
 namespace licenser::configmgr {
 bool ConfigWriter::write(ApplicationArgs app, std::string path) {
   std::filesystem::path p(path);
@@ -44,15 +48,19 @@ bool ConfigWriter::write(ApplicationArgs app, std::string path) {
   stream << "# Like author=Naruto, Sasuke, Sakura\n";
   stream << "# You can also write an Organization name here like Tensorflow "
             "Team or Similar\n";
-  stream << "author=" << app.author << '\n' << '\n';
+  stream << LICENSER_RC_DECORATOR;
+  stream << "author=" << app.author << '\n';
+  stream << LICENSER_RC_DECORATOR;
 
-  stream << "# This is the email of the author (optional)\n";
+  stream << "\n# This is the email of the author (optional)\n";
   stream << "# It is used in License headers. It can be comma separated for "
             "multiple values\n";
   stream << "# You can leave it empty by writing email= \n";
-  stream << "email=" << app.email << '\n' << '\n';
+  stream << LICENSER_RC_DECORATOR;
+  stream << "email=" << app.email << '\n';
+  stream << LICENSER_RC_DECORATOR;
 
-  stream << "# This is the short name for the license.\n";
+  stream << "\n# This is the short name for the license.\n";
   stream << "# We follow standard shortnames for the Licenses.\n";
   stream << "# This is case-insensitive and shortname spaces are replaced with "
             "\"_\"\n";
@@ -62,28 +70,36 @@ bool ConfigWriter::write(ApplicationArgs app, std::string path) {
             "indeed a license shortname\n";
   stream << "# A list of all the shortnames can be retrieved by running "
             "licenser --show_licenses\n";
-  stream << "license=" << app.license << '\n' << '\n';
+  stream << LICENSER_RC_DECORATOR;  
+  stream << "license=" << app.license << '\n';
+  stream << LICENSER_RC_DECORATOR;
 
-  stream << "# This value tells that the project is still under active and \n";
+  stream << "\n# This value tells that the project is still under active and \n";
   stream << "# this is used to set the period of the project in the license "
             "header\n";
   stream << "# If a project was started in 2018 and is still going in 2019, "
             "setting this\n";
   stream << "# value to true makes sure that in license header we write "
             "2018-2019\n";
+  stream << LICENSER_RC_DECORATOR;
   stream << "ongoing_project=" << (app.ongoing_project ? "true" : "false")
-         << '\n'
          << '\n';
+  stream << LICENSER_RC_DECORATOR;
 
-  stream << "# This is the name of the project, It is required and should be "
+  stream << "\n# This is the name of the project, It is required and should be "
             "non-empty\n";
   stream << "# This value is also used to write about project in the license "
             "header.\n";
-  stream << "project=" << app.project << '\n' << '\n';
+  stream << LICENSER_RC_DECORATOR;
+  stream << "project=" << app.project << '\n';
+  stream << LICENSER_RC_DECORATOR;
 
-  stream << "# This is the starting year of your project\n";
+  stream << "\n# This is the starting year of your project\n";
   stream << "# It must be after 1900 and cannot exceed your current year\n";
+  
+  stream << LICENSER_RC_DECORATOR;
   stream << "year=" << app.year << '\n';
+  stream << LICENSER_RC_DECORATOR;
   stream.close();
   return true;
 }
